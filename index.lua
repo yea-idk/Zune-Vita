c224f1f605faf0305025c3afca8fd213ec341800
yea-idk <62530097+yea-idk@users.noreply.github.com>
2020-08-07 17:23:51 -0400

    mostly fix image loading

--- index.lua
+++ index.lua
@@ -1,8 +1,9 @@
---v0.99
+--v0.113
 -- i have no idea what im doing anymore
 function appcull()
-	if (offset < 0) then
-		offset = 0
+	tmp = 0
+	if (offset < 1) then
+		offset = 1
 	elseif (offset > #apps - 4) then
 		offset = #apps - 4
 	end
@@ -13,18 +14,18 @@ function appcull()
 			apptype = mobile
 		end
 		if (System.doesFileExist(apptype .. apps[offset + tmp] .. '/icon0.png')) then
-			app0 = Graphics.loadImage(apptype .. apps[offset + tmp] .. '/icon0.png')
+			app0 = apptype .. apps[offset + tmp] .. '/icon0.png'
 		else
-			app0 = Graphics.loadImage('app0:/app/noicon.png')
+			app0 = 'app0:/app/noicon.png'
 		end
-		if (tmp == 1) then
-			app1 = app0
+		if (tmp == 0) then
+			app1 = Graphics.loadImage(app0)
+		elseif (tmp == 1) then
+			app2 = Graphics.loadImage(app0)
 		elseif (tmp == 2) then
-			app2 = app0
+			app3 = Graphics.loadImage(app0)
 		elseif (tmp == 3) then
-			app3 = app0
-		elseif (tmp == 4) then
-			app4 = app0
+			app4 = Graphics.loadImage(app0)
 		end
 		tmp = tmp + 1
 	end
@@ -40,7 +41,7 @@ font2 = Font.load('app0:/app/Meiryo.ttf')
 white = Color.new(255, 255, 255)
 grey = Color.new(87, 87, 87)
 black = Color.new(0, 0, 0)
-y = -100
+y = -125
 vita = 'ur0:/appmeta/'
 mobile = 'app0:/app/'
 
@@ -99,11 +100,6 @@ update = 1
 x = y
 offset = 0
 change = 0
-cull = 10
-tt = 31
-tmp = 1
-x3 = 0
-y3 = 0
 appcull()
 Font.setPixelSizes(font2, 100)
 Font.setPixelSizes(font, 15)
@@ -123,62 +119,43 @@ while true do
 			first = 1
 		end
 	end
-	if (x1 ~= nil) then -- taps
-		x3 = x1
-		y3 = y1
-		if (first1 == 1) then
-			first1 = 0
-			tt = 0
-		else
-			tt = tt + 0.1
-		end
-	end
-	if (tt <= 30) and (x1 == nil) then
-		tt = 0
-		first1 = 1
-		if (x3 <= 281) then
-			colour = Screen.getPixel(x3, y3)
-			if (colour ~= 000000) or (colour ~= 255255255) then
-				idk = 1
-			end
-		end
-	end
-	if (x - y <= -175) then -- track touch for culling
+	if (x - y <= -175 * 3) then -- track touch for culling
 		update = 1
-		offset = offset - 1
-	elseif (x - y >= 175) then
+		offset = offset - 3
+	elseif (x - y >= 175 * 3) then
 		update = 1
-		offset = offset + 1
+		offset = offset + 3
 	end
 	Graphics.initBlend()
 	Screen.clear()
 	tmp = 1
 	tmp1 = 1
-	for i = 1, #apps do -- ultra inefficient
+	for i = 1, #apps do
 		if not (y + (281 * tmp) > 825) then
 			if not (y + (281 * tmp) < -281) then
-				Graphics.drawScaleImage(25, y + (281 * tmp), bgicon, 2, 2) -- draw this always because of games like jet set radio
-				cull = cull - 0.1
-				if (offset == 0) and (cull <= 0) then
+				if (offset == 0) then
 					offset = tmp
-					cull = 10
 				end
-				Graphics.drawScaleImage(25, y + (281 * tmp), app1, 2, 2)
-				Graphics.drawScaleImage(25, y + (281 * (tmp + 1)), app2, 2, 2)
-				Graphics.drawScaleImage(25, y + (281 * (tmp + 2)), app3, 2, 2)
-				Graphics.drawScaleImage(25, y + (281 * (tmp + 3)), app4, 2, 2)
+				tmp1 = 1
+				for j = 1, 4 do
+					Graphics.drawScaleImage(25, y + (281 * tmp1), bgicon, 2, 2)
+					tmp1 = tmp1 + 1
+				end
+				Graphics.drawScaleImage(25, y + (281 * offset), app1, 2, 2)
+				Graphics.drawScaleImage(25, y + (281 * 2 * offset), app2, 2, 2)
+				Graphics.drawScaleImage(25, y + (281 * 3 * offset), app3, 2, 2)
+				Graphics.drawScaleImage(25, y + (281 * 4 * offset), app4, 2, 2)
 				Font.print(font, 288, y + (281 * tmp), appdesc[tmp], grey)
 			end
 		end
 		tmp = tmp + 1
 	end
 	if (update == 1) then -- app culling
-		tmp = 1
 		appcull()
 	end
 	Graphics.fillRect(0, 960, 0, 45, black)
 	Font.print(font2, -24, -60, "apps", white)
-	Graphics.debugPrint(0, 0, Screen.getPixel(x3, y3), grey)
+	Graphics.debugPrint(0, 0, apps[offset] .. apps[offset + 1] .. apps[offset + 2] .. apps[offset + 3], grey)
 	Graphics.termBlend()
 	Screen.flip()
 end
